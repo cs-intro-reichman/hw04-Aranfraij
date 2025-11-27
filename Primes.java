b8dbcefb6c982e5f2ec56df734e4f1cea2814667
Aranfraij <aran.fraij@post.runi.ac.il>
2025-11-27 18:00:04 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -1,35 +1,29 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        boolean prime [] = new boolean[n+1];
-        int i = 2;
-        int j = 2;
-        int num = 2;
-        while (i <= n) {
-            prime [i] = true;
-            i++;
-        }
-        while (j <= Math.sqrt(n)) {
-            if (prime[j] == true) {
-                num = j +j;
-                while (num <= n) {
-                    prime[num] = false;
-                    num = num +j;
+        boolean[] arr = new boolean[n+1];
+        arr[0]=false;
+        arr[1]=false;
+        for (int i=2;i<=n;i++){ //assign true to every array spot
+            arr[i]=true;
+        } 
+        for (int i=2; i<=n;i++){
+            for(int j=i+1;j<=n;j++){
+                if (j%i==0){
+                    arr[j]=false;
                 }
             }
-            j++;
         }
-        j = 0;
-        num = 0;
-        System.out.println("Prime nombers up to " + n + ":");
-        while (num <= n) {
-            if (prime[num] == true) {
-                System.out.println(num);
-                j++;
+        int count=0;
+        System.out.println("Prime numbers up to "+n+":");
+        for (int i=2;i<=n;i++){
+            if(arr[i]==true){
+                System.out.println(i);
+                count++;
             }
-            num++;     
         }
-        double precent = (((double) j / (double) n) * 100);
-        System.out.println("There are " + j + " primes between 2 and " + n + " (" + (int)precent + "% are primes)");
+            double precent = Math.floor(((double)count/n)*100);
+            System.out.print("There are "+count+" primes between 2 and " +n);
+            System.out.print(" ("+(int)precent+"% are primes)");
     }
 }
